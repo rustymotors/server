ef40876d686483a517fe25896b000ff545e65bf8
Joseph W Becher <jwbecher@drazisil.com>
2016-03-06 18:10:59 -0500

    adjusted for 0.10

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-const dgram = require('dgram')
-const server = dgram.createSocket('udp4')
+var dgram = require('dgram')
 
-server.on('error', (err) => {
-  console.log(`server error:\n${err.stack}`)
+var server = dgram.createSocket('udp4')
+
+server.on('error', function (err) {
+  console.log(`server error:\n` + err.stack)
   server.close()
 })
 
-server.on('message', (msg, rinfo) => {
-  console.log(`'server got: ${msg} from ${rinfo.address}:${rinfo.port}`)
+server.on('message', function (msg, rinfo) {
+  console.log('server got: ' + msg + ' from ' +
+    rinfo.address + ':' + rinfo.port)
 })
 
-server.on('listening', () => {
+server.on('listening', function () {
   var address = server.address()
-  console.log(`server listening ${address.address}:${address.port}`)
+  console.log('server listening ' +
+      address.address + ':' + address.port)
 })
 
 server.bind(8226)
-// server listening 0.0.0.0:8226
+// server listening 0.0.0.0:41234
