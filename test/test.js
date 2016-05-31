286b3c127d6cdbeee6b4c5880345bf40d395d5da
Joseph W Becher <jwbecher@drazisil.com>
2016-05-31 08:22:05 +0000

    Test shell

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require('chai').assert;
+describe('Files', function() {
+  it('should return -1 when the value is not present');
+});
+
+describe('Databases', function() {
+  it('should return -1 when the value is not present');
+});
+
+describe('Connections', function() {
+  it('should return -1 when the value is not present');
+});
+
+describe('Encryption', function() {
+  it('should return -1 when the value is not present');
+});
+
+describe('Commands', function() {
+  it('should return -1 when the value is not present');
+});
