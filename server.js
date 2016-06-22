1732078960cc0a176ceb5cca55754c2f764c7402
Joseph W Becher <jwbecher@drazisil.com>
2016-06-22 08:22:53 -0400

    Added MCOTS port and sorted nps files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var mcoServer = require('./src/mco_server/index.js')
 
-var SERVER_PORTS = ['8226', '8228', '7003']
+// port 43300 = MCOTS
+var SERVER_PORTS = ['8226', '8228', '7003', '43300']
 
 mcoServer.init()
 mcoServer.start(SERVER_PORTS)
