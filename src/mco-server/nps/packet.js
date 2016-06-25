f76e3ebc9cb56bc5dc5cd0439ccf1a0810fb2f3b
Joseph W Becher <jwbecher@drazisil.com>
2016-06-25 15:54:41 -0400

    start of packet builder

---
+++ src/mco-server/nps/packet.js
@@ -0,0 +1,20 @@
+var crypto = require('crypto')
+
+function buildPacket (len, header, content) {
+  var packet = new Buffer(len)
+
+  // Fill packet with random bytes
+  packet = crypto.randomBytes(packet.length)
+
+  // Add the response code
+  packet.writeUInt16BE(header)
+
+  // Write the content
+  content.copy(packet, 2)
+
+  return packet
+}
+
+module.exports = {
+  buildPacket: buildPacket
+}
