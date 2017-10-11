4a76677cfe3ac2cafede2048ddd0ecb677acc15a
Drazisil <jwbecher@drazisil.com>
2017-10-11 17:58:57 -0400

    Added tests for packet and npsOpcodes

---
+++ src/packet.test.js
@@ -0,0 +1,17 @@
+const packet = require('./packet.js')
+
+test('retuns a buffer packet from premadeLogin', () => {
+    expect(packet.premadeLogin()).toBeInstanceOf(Buffer)
+})
+
+test('retuns a buffer packet from craftGenericReply', () => {
+    expect(packet.craftGenericReply()).toBeInstanceOf(Buffer)
+})
+
+test('retuns a buffer packet from premadePersonaMaps', () => {
+    expect(packet.premadePersonaMaps()).toBeInstanceOf(Buffer)
+})
+
+test('retuns a buffer packet from buildPacket', () => {
+    expect(packet.buildPacket(6, 0x601, new Buffer.from([0x06, 0x01]))).toBeInstanceOf(Buffer)
+})
