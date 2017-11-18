70fef2da213b850f451aa4a97faf1f716f909108
Joseph W Becher <jwbecher@drazisil.com>
2017-11-18 15:08:11 -0500

    Should be clener now

--- tests/packet.test.js
+++ tests/packet.test.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-const packet = require("./packet.js");
+const packet = require("../src/packet.js");
 
 test("retuns a buffer packet from premadeLogin", () => {
   expect(packet.premadeLogin()).toBeInstanceOf(Buffer);
