2df061d28a5dd52e9da63c6c0c495e6925a2f6cf
Joseph W Becher <drazisil@users.noreply.github.com>
2017-11-18 14:45:30 -0500

    Moved tests to tests dir (#38)

--- src/MCServer.js
+++ src/MCServer.js
@@ -1,5 +1,5 @@
 // mco-server is a game server, written from scratch, for an old game
-// Copyright (C) <2017-2018>  <Joseph W Becher> 
+// Copyright (C) <2017-2018>  <Joseph W Becher>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-/* Internal dependencies */
 const readline = require("readline");
 const net = require("net");
 const fs = require("fs");
