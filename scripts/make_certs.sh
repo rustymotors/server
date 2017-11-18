0aeb8d92ecf589758ae8f30f7cb6b34b70bf6482
Joseph W Becher <drazisil@users.noreply.github.com>
2017-11-18 14:41:07 -0500

    Rename mcouniverse.cnf to data/mcouniverse.cnf (#37)
    * Rename mcouniverse.cnf to data/mcouniverse.cnf
    
    * moved cnf file

--- scripts/make_certs.sh
+++ scripts/make_certs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openssl req -x509 -config mcouniverse.cnf -newkey rsa:1024 -nodes -keyout ./data/private_key.pem -out ./data/cert.pem -days 365
+openssl req -x509 -config data/mcouniverse.cnf -newkey rsa:1024 -nodes -keyout ./data/private_key.pem -out ./data/cert.pem -days 365
 openssl rsa -in ./data/private_key.pem -outform DER -pubout -out ./data/pub.key
