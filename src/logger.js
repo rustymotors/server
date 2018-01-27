835984b4a872edb1641122fdc4030292e62305d8
Joseph W Becher <jwbecher@drazisil.com>
2018-01-26 20:00:01 -0500

    Added dotenv and started making login server a standalone module

--- src/logger.js
+++ src/logger.js
@@ -15,7 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const logger = require('winston');
-const config = require('../config/config.json');
+require('dotenv').config();
 
 logger.cli();
 logger.add(require('winston-daily-rotate-file'), {
@@ -25,6 +25,6 @@ logger.add(require('winston-daily-rotate-file'), {
   datePattern: 'yyyy-MM-dd_',
 });
 
-logger.level = config.loggerLevel;
+logger.level = process.env.LOGGER_LEVEL || 'error';
 
 module.exports = logger;
