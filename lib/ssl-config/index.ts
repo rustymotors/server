0875274b6ea57b439cbe292d35c8e27460957c2b
Joseph W Becher <jwbecher@drazisil.com>
2018-02-13 18:26:55 -0500

    Add credit

--- lib/ssl-config/index.ts
+++ lib/ssl-config/index.ts
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+// This is a subset of https://github.com/certsimple/ssl-config
+
 export default class OldSSLCiphers {
 
   public ciphers: string;
