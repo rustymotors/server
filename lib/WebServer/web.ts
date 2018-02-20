95802f0f05753bc9d4e1fc17413e57c1673ca679
Joseph W Becher <jwbecher@drazisil.com>
2018-02-20 18:54:42 -0500

    Add routes

--- lib/WebServer/web.ts
+++ lib/WebServer/web.ts
@@ -17,7 +17,7 @@
 import fs = require("fs");
 import * as http from "http";
 import * as https from "https";
-import { config as configurationFile, IConfigurationFile } from "../../config/config";
+import { config, IConfigurationFile } from "../../config/config";
 import SSLConfig from "../ssl-config";
 
 import { Socket } from "net";
@@ -25,15 +25,14 @@ import { logger } from "../../src/logger";
 
 /**
  * Create the SSL options object
- * @param {JSON} config
  */
-function sslOptions(config: IConfigurationFile["serverConfig"]) {
+function sslOptions(configuration: IConfigurationFile["serverConfig"]) {
   const sslConfig = new SSLConfig();
   return {
-    cert: fs.readFileSync(config.certFilename),
+    cert: fs.readFileSync(configuration.certFilename),
     ciphers: sslConfig.ciphers,
     honorCipherOrder: true,
-    key: fs.readFileSync(config.privateKeyFilename),
+    key: fs.readFileSync(configuration.privateKeyFilename),
     rejectUnauthorized: false,
     secureOptions: sslConfig.minimumTLSVersion,
   };
@@ -41,79 +40,43 @@ function sslOptions(config: IConfigurationFile["serverConfig"]) {
 
 function httpsHandler(request: http.IncomingMessage, response: http.ServerResponse) {
   logger.info(`Request from ${request.socket.remoteAddress} for ${request.method} ${request.url}`)
+  switch (request.url) {
+    case "/key":
+      response.setHeader("Content-disposition", "attachment; filename=cert.pem");
+      response.end(fs.readFileSync(config.serverConfig.publicKeyFilename).toString("hex"));
+      break;
+    case "/AuthLogin":
+      response.setHeader("Content-Type", "text/plain")
+      response.end("Valid=TRUE\nTicket=d316cd2dd6bf870893dfbaaf17f965884e");
+      break;
+
+
+    default:
+      response.statusCode = 404;
+      response.end("Unknown request.")
+      break;
+  }
 }
 
 export default class WebServer {
 
   public async start() {
 
-    const config = configurationFile.serverConfig;
-
     const httpsServer = https
-    .createServer(sslOptions(config), httpsHandler)
-    .listen({ port: 443, host: "0.0.0.0"})
-  .on("connection", (socket: Socket) => {
-    // logger.info("New SSL connection");
-    socket.on("error", (error: NodeJS.ErrnoException) => {
-      logger.error(`SSL Socket Error: ${error.message}`);
-    });
-    socket.on("close", () => {
-      // logger.info("SSL Socket Connection closed");
-    });
-  })
-  .on("tlsClientError", (err: Error) => {
-    logger.error(`tlsClientError: ${err}`);
-  });
+      .createServer(sslOptions(config.serverConfig), httpsHandler)
+      .listen({ port: 443, host: "0.0.0.0" })
+      .on("connection", (socket: Socket) => {
+        // logger.info("New SSL connection");
+        socket.on("error", (error: NodeJS.ErrnoException) => {
+          logger.error(`SSL Socket Error: ${error.message}`);
+        });
+        socket.on("close", () => {
+          // logger.info("SSL Socket Connection closed");
+        });
+      })
+      .on("tlsClientError", (err: Error) => {
+        logger.error(`tlsClientError: ${err}`);
+      });
 
+  }
 }
-}
-
-/**
- * Create the HTTP seb server
- * @param {Function} callback
- */
-
-  
-
-  // app.use(bodyParser.json()); // support json encoded bodies
-  // app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-  // /**
-  //  * Return the public vert
-  //  */
-  // app.get("/key", (req, res) => {
-  //   res.setHeader("Content-disposition", "attachment; filename=cert.pem");
-  //   res.write(fs.readFileSync(config.publicKeyFilename).toString("hex"));
-  //   res.end();
-  // });
-
-  // /**
-  //  * Return the public key
-  //  */
-  // app.get("/key", (req, res) => {
-  //   res.setHeader("Content-disposition", "attachment; filename=pub.key");
-  //   res.write(fs.readFileSync(config.publicKeyFilename).toString("hex"));
-  //   res.end();
-  // });
-
-  // /**
-  //  * This endpoint receives the username and password
-  //  */
-  // app.get("/AuthLogin", (req, res) => {
-  //   res.set("Content-Type", "text/plain");
-  //   res.send("Valid=TRUE\nTicket=d316cd2dd6bf870893dfbaaf17f965884e");
-  // });
-
-  // app.use((req, res) => {
-  //   logger.debug("SSL");
-  //   logger.debug("Headers: ", req.headers);
-  //   logger.debug(`Method: ${req.method}`);
-  //   logger.debug(`Url: ${req.url}`);
-  //   res.send("404");
-  // });
-
-
-
-
-
-
