e9cf7cd437775cf31867164d97f3dfe6a1e11996
Joe Becher <drazisil@users.noreply.github.com>
2018-02-21 14:07:36 -0500

    Test coverage (#60)
    * Add some tests for Connection class
    
    * add exports
    
    * moved setEncryptionKey to Connectin class and wrote test
    
    * refactor cipher methods into connection class and write tests

--- src/Connection.ts
+++ src/Connection.ts
@@ -1,3 +1,20 @@
+// mco-server is a game server, written from scratch, for an old game
+// Copyright (C) <2017-2018>  <Joseph W Becher>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import * as crypto from "crypto";
 import { Cipher, Decipher } from "crypto";
 import { Socket } from "net";
 import ConnectionMgr from "./connectionMgr";
@@ -9,15 +26,15 @@ export class Connection {
   public id: number;
   public inQueue: boolean;
   public enc: {
-    cypher?: Cipher,
+    cipher?: Cipher,
     decipher?: Decipher
   };
   public useEncryption: boolean;
   public isSetupComplete: boolean;
   public decryptedCmd: Buffer;
   public encryptedCommand: Buffer;
+  public status: string;
   private appID: number;
-  private status: string;
   private msgEvent: null;
   private lastMsg: number;
   private mgr: ConnectionMgr;
@@ -36,5 +53,28 @@ export class Connection {
     this.isSetupComplete = false;
     this.mgr = mgr;
     this.inQueue = true;
+  }
+
+  /**
+   * setEncryptionKey
+   */
+  public setEncryptionKey(sessionKey: string) {
+    this.enc.cipher = crypto.createCipheriv("rc4", sessionKey, "");
+    this.enc.decipher = crypto.createDecipheriv("rc4", sessionKey, "");
+    this.isSetupComplete = true;
+  }
+
+  /**
+   * CipherBuffer
+   */
+  public cipherBuffer(messageBuffer: Buffer) {
+    return this.enc.cipher.update(messageBuffer);
+  }
+  
+  /**
+   * DecipherBuffer
+   */
+  public decipherBuffer(messageBuffer: Buffer) {
+    return this.enc.decipher.update(messageBuffer);
   }
 }
