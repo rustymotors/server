09e520358abdd301cc6457d9eae44898393ea5e1
Joseph W Becher <jwbecher@drazisil.com>
2018-04-13 12:09:42 -0400

    move back to using winston for logging

--- src/logger.ts
+++ src/logger.ts
@@ -15,38 +15,64 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import * as config from "dotenv";
+import * as winston from "winston"
 
-export interface ILoggerOptions {
-  level: string;
-  filename?: string;
-  datePattern?: string;
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({
+      filename: 'filelog-info.log',
+      level: 'info',
+      name: 'info-file',
+    }),
+    new (winston.transports.File)({
+      filename: 'filelog-error.log',
+      level: 'error',
+      name: 'error-file',
+    })
+  ]
+});
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+// 
+if (process.env.NODE_ENV !== 'production') {
+  logger.cli();
 }
 
-export class Logger {
-  private level: string;
+// export interface ILoggerOptions {
+//   level: string;
+//   filename?: string;
+//   datePattern?: string;
+// }
 
-  constructor(options: ILoggerOptions) {
-    this.level = options.level;
-  }
+// export class Logger {
+//   private level: string;
 
-  public error(...message: string[]) {
-    process.stderr.write(`[ERROR] ${message.join(' ')} \n`);
-  }
+//   constructor(options: ILoggerOptions) {
+//     this.level = options.level;
+//   }
 
-  public debug(...message: string[]) {
-    process.stdout.write(`[DEBUG] ${message.join(' ')} \n`);
-  }
+//   public error(...message: string[]) {
+//     process.stderr.write(`[ERROR] ${message.join(' ')} \n`);
+//   }
 
-  public info(...message: string[]) {
-    process.stdout.write(`[INFO] ${message.join(' ')} \n`);
-  }
+//   public debug(...message: string[]) {
+//     process.stdout.write(`[DEBUG] ${message.join(' ')} \n`);
+//   }
 
-  public warn(...message: string[]) {
-    process.stdout.write(`[WARN] ${message.join(' ')} \n`);
-  }
+//   public info(...message: string[]) {
+//     process.stdout.write(`[INFO] ${message.join(' ')} \n`);
+//   }
 
-}
+//   public warn(...message: string[]) {
+//     process.stdout.write(`[WARN] ${message.join(' ')} \n`);
+//   }
+
+// }
 
 
-export const logger = new Logger({ level: process.env.LOGGER_LEVEL || "error" });
+// export const logger = new Logger({ level: process.env.LOGGER_LEVEL || "error" });
+
+export { logger }
 
