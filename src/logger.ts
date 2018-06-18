8db4bfe441888ff1979fb9a6dba111d0a158e788
Drazisil <jwbecher@drazisil.com>
2018-06-18 11:57:24 -0400

    corrected logging

--- src/logger.ts
+++ src/logger.ts
@@ -14,66 +14,21 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import * as config from "dotenv";
-import * as winston from "winston";
-
-const logger = new winston.Logger({
-  // tslint:disable-next-line:no-bitwise
-  level: "debug",
+import { Logger, transports } from "winston";
+const logger = new Logger({
   transports: [
-    new winston.transports.File({
+    new transports.Console({ level: "debug" }),
+    new transports.File({
       filename: "filelog-info.log",
       level: "info",
       name: "info-file",
     }),
-    new winston.transports.File({
+    new transports.File({
       filename: "filelog-error.log",
       level: "error",
       name: "error-file",
     }),
-    new winston.transports.Console(),
   ],
 });
-
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== "production") {
-  logger.cli();
-}
-
-// export interface ILoggerOptions {
-//   level: string;
-//   filename?: string;
-//   datePattern?: string;
-// }
-
-// export class Logger {
-//   private level: string;
-
-//   constructor(options: ILoggerOptions) {
-//     this.level = options.level;
-//   }
-
-//   public error(...message: string[]) {
-//     process.stderr.write(`[ERROR] ${message.join(' ')} \n`);
-//   }
-
-//   public debug(...message: string[]) {
-//     process.stdout.write(`[DEBUG] ${message.join(' ')} \n`);
-//   }
-
-//   public info(...message: string[]) {
-//     process.stdout.write(`[INFO] ${message.join(' ')} \n`);
-//   }
-
-//   public warn(...message: string[]) {
-//     process.stdout.write(`[WARN] ${message.join(' ')} \n`);
-//   }
-
-// }
-
-// export const logger = new Logger({ level: process.env.LOGGER_LEVEL || "error" });
 
 export { logger };
