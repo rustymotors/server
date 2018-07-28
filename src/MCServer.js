a51abbb2493caf6ca55161aaff32b0a0b2782e9d
Joe Becher <jwbecher@gmail.com>
2018-07-27 20:20:05 -0400

    seperate db connection from calls

--- src/MCServer.js
+++ src/MCServer.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-const database = require('../lib/database');
+const database = require('../lib/database/db');
 const { ConnectionMgr } = require('./connectionMgr');
 const { startTCPListener } = require('./listenerThread');
 const { logger } = require('./logger');
