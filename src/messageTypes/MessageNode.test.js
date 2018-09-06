46a2422e88516414efab3521cabc7b7ac9687658
Joe Becher <jwbecher@drazisil.com>
2018-09-05 21:36:48 -0400

    re-licence as MPL

--- src/messageTypes/MessageNode.test.js
+++ src/messageTypes/MessageNode.test.js
@@ -1,20 +1,11 @@
 // mco-server is a game server, written from scratch, for an old game
 // Copyright (C) <2017-2018>  <Joseph W Becher>
 
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-const { MessageNode } = require('./MessageNode');
+const { MessageNode } = require("./MessageNode");
 
 const messageNode1 = new MessageNode();
 messageNode1.deserialize(
@@ -41,8 +32,9 @@ messageNode1.deserialize(
     0x00,
     0x00,
     0x00,
-  ]));
+  ])
+);
 
-test('packet is a MCOTS packet', () => {
+test("packet is a MCOTS packet", () => {
   expect(messageNode1.isMCOTS()).toBe(true);
 });
