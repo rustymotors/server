2c479bf7ed3a04028c8d6d0d09b3ab15c6aea7ab
Joe Becher <jwbecher@drazisil.com>
2019-07-06 10:13:20 -0400

    log file in json with logrotate

--- src/services/shared/logger.ts
+++ src/services/shared/logger.ts
@@ -6,26 +6,29 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import * as winston from "winston";
+import * as DailyRotateFile from "winston-daily-rotate-file";
 
 // tslint:disable-next-line:no-empty-interface
 export interface ILoggerInstance extends winston.Logger {}
 
 export class Logger {
-  public level: string;
+  public loggingLevel: string;
   public logger: winston.Logger;
 
   constructor(level: string = "debug") {
-    this.level = level;
+    this.loggingLevel = level;
+    const consoleFormat = winston.format.combine(
+      winston.format.colorize({ all: true }),
+      winston.format.simple()
+    );
     this.logger = winston.createLogger({
-      format: winston.format.combine(
-        winston.format.colorize({ all: true }),
-        winston.format.simple()
-      ),
       transports: [
-        new winston.transports.Console({ level }),
-        new winston.transports.File({
-          filename: "logs/mcoServer.log",
+        new winston.transports.Console({ level, format: consoleFormat }),
+        new DailyRotateFile({
+          filename: "logs/mcoServer-%DATE%.log",
           level,
+          datePattern: "YYYY-MM-DD-HH",
+          zippedArchive: true,
         }),
       ],
     });
