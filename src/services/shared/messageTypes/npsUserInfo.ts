68b6bb6057d6bbfbdb9e09c188c2c5da3c4a1b77
Joe Becher <drazisil@users.noreply.github.com>
2019-07-13 20:43:05 -0400

    Improve test coverage (#205)
    * do not call from constructor
    
    This allows me to create a testable instance with out extractSessionKeyFromPacket
    
    * lowercase
    
    * initial tests
    
    * pass logger to constructor
    
    * initial tests
    
    * remove duplidate coverage report

--- src/services/shared/messageTypes/npsUserInfo.ts
+++ src/services/shared/messageTypes/npsUserInfo.ts
@@ -5,26 +5,30 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { Logger } from "../logger";
-
-const logger = new Logger().getLogger();
+import { ILoggers } from "../logger";
 
 export class NPSUserInfo {
+  public loggers: ILoggers;
   public userId: number;
   public userName: Buffer; // 30 length
   public userData: Buffer; // 64 length;
 
-  constructor(rawData: Buffer) {
+  constructor(rawData: Buffer, loggers: ILoggers) {
     this.userId = rawData.readInt32BE(4);
     this.userName = rawData.slice(8, 38);
     this.userData = rawData.slice(38);
+    this.loggers = loggers;
   }
 
   public dumpInfo() {
-    logger.debug("[NPSUserInfo]======================================");
-    logger.debug(`UserId:        ${this.userId}`);
-    logger.debug(`UserName:      ${this.userName.toString()}`);
-    logger.debug(`UserData:      ${this.userData.toString("hex")}`);
-    logger.debug("[/NPSUserInfo]======================================");
+    this.loggers.both.debug(
+      "[NPSUserInfo]======================================"
+    );
+    this.loggers.both.debug(`UserId:        ${this.userId}`);
+    this.loggers.both.debug(`UserName:      ${this.userName.toString()}`);
+    this.loggers.both.debug(`UserData:      ${this.userData.toString("hex")}`);
+    this.loggers.both.debug(
+      "[/NPSUserInfo]======================================"
+    );
   }
 }
