b7207c4d7594dd49ed503177ff0aba7987d83304
Joe Becher <drazisil@users.noreply.github.com>
2019-07-14 00:44:28 -0400

    remove express (#208)
    refactor the authlogin/web server to be easier to test

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import { Logger } from "./services/shared/logger";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
 
 // get instance of loggers
 const loggers = new Logger().getLoggers();
 
+// get instance of config
+const config = yaml.safeLoad(
+  fs.readFileSync("./src/services/shared/config.yml", "utf8")
+);
+
 // MCOS Monolith
 import { Server } from "./server";
-const server = new Server(loggers);
+const server = new Server(config, loggers);
 // MCOS AuthLogin and Shard
-import * as AuthLogin from "./services/AuthLogin/app";
-AuthLogin.start(loggers);
+import { WebServer } from "./services/AuthLogin/WebServer";
+const AuthLogin = new WebServer(config, loggers);
+AuthLogin.start();
 // MCOS PatchAndShard
 import * as PatchAndShard from "./services/PatchAndShard/app";
 PatchAndShard.start(loggers);
