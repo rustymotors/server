445a41f84e2b5ead5dad296f5dd89ca230fb5ac8
Joe Becher <jwbecher@drazisil.com>
2019-07-16 18:37:11 -0400

    dynamicly create the registry file

---
+++ src/services/AuthLogin/WebServer.test.ts
@@ -0,0 +1,22 @@
+import { WebServer } from "./WebServer";
+import { Logger } from "../shared/logger";
+import * as fs from "fs";
+
+// get instance of loggers
+const loggers = new Logger().getLoggers();
+
+// get instance of config
+const config = JSON.parse(
+  fs.readFileSync("./src/services/shared/config.json", "utf8")
+);
+
+describe("WebServer", () => {
+  test("can generate registry file", () => {
+    const webServer = new WebServer(config, loggers);
+    const staticRegistry = fs.readFileSync(
+      config.serverConfig.registryFilename
+    );
+    const dynamicRegistry = webServer._handleGetRegistry();
+    expect(dynamicRegistry).toEqual(staticRegistry.toString("UTF-8"));
+  });
+});
