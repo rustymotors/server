c85760468f3179639216140efb34c34e6039c990
Joe Becher <jwbecher@drazisil.com>
2019-07-20 08:15:38 -0400

    saving

--- src/services/shared/messageTypes/NPSMsg.ts
+++ src/services/shared/messageTypes/NPSMsg.ts
@@ -21,6 +21,17 @@ export enum MSG_DIRECTION {
 }
 
 export class NPSMsg {
+  public msgNameMapping = [
+    { id: 0x120, name: "NPS_LOGIN_RESP" },
+    { id: 0x128, name: "NPS_GET_MINI_USER_LIST" },
+    { id: 0x207, name: "NPS_ACK" },
+    { id: 0x229, name: "NPS_MINI_USER_LIST" },
+    { id: 0x30c, name: "NPS_SEND_MINI_RIFF_LIST" },
+    { id: 0x501, name: "NPS_USER_LOGIN" },
+    { id: 0x503, name: "NPS_REGISTER_GAME_LOGIN" },
+    { id: 0x532, name: "NPS_GET_PERSONA_MAPS" },
+    { id: 0x607, name: "NPS_GAME_ACCOUNT_INFO" },
+  ];
   public msgNo: number;
   protected msgLength: number;
   protected msgVersion: number;
@@ -37,6 +48,13 @@ export class NPSMsg {
     this.direction = direction;
   }
 
+  public msgCodetoName(msgId: number) {
+    const mapping = this.msgNameMapping.find(mapping => {
+      return mapping.id === msgId;
+    });
+    return mapping ? mapping.name : "Unknown msgId";
+  }
+
   public setContent(buffer: Buffer) {
     this.content = buffer;
     this.msgLength = this.content.length + 12;
@@ -51,14 +69,22 @@ export class NPSMsg {
   }
 
   public serialize() {
-    const packet = Buffer.alloc(this.msgLength);
-    packet.writeInt16BE(this.msgNo, 0);
-    packet.writeInt16BE(this.msgLength, 2);
-    packet.writeInt16BE(this.msgVersion, 4);
-    packet.writeInt16BE(this.reserved, 6);
-    packet.writeInt32BE(this.msgLength, 8);
-    this.content.copy(packet, 12);
-    return packet;
+    try {
+      const packet = Buffer.alloc(this.msgLength);
+      packet.writeInt16BE(this.msgNo, 0);
+      packet.writeInt16BE(this.msgLength, 2);
+      if (this.msgLength > 4) {
+        packet.writeInt16BE(this.msgVersion, 4);
+        packet.writeInt16BE(this.reserved, 6);
+      }
+      if (this.msgLength > 8) {
+        packet.writeInt32BE(this.msgLength, 8);
+        this.content.copy(packet, 12);
+      }
+      return packet;
+    } catch (error) {
+      throw new Error(`[NPSMsg] Error in serialize(): ${error}`);
+    }
   }
 
   public deserialize(packet: Buffer) {
@@ -69,14 +95,24 @@ export class NPSMsg {
     return this;
   }
 
+  public dumpPacketHeader(messageType: string) {
+    logger.debug(
+      `[NPSMsg/${messageType}] == ${this.direction} ==================`
+    );
+    logger.debug(
+      `MsgNo:         ${this.msgNo.toString(16)} (${
+        this.msgNo
+      }) [${this.msgCodetoName(this.msgNo)}]`
+    );
+    logger.debug(`MsgVersion:    ${this.msgVersion}`);
+    logger.debug(`contentLength: ${this.msgLength}`);
+  }
+
   /**
    * dumpPacket
    */
   public dumpPacket() {
-    logger.debug(`[NPSMsg] == ${this.direction} ==================`);
-    logger.debug(`MsgNo:         ${this.msgNo.toString(16)} (${this.msgNo})`);
-    logger.debug(`MsgVersion:    ${this.msgVersion}`);
-    logger.debug(`contentLength: ${this.msgLength}`);
+    this.dumpPacketHeader("NPSMsg");
     logger.debug(`Content:       ${this.content.toString("hex")}`);
     logger.debug(`Serialized:    ${this.serialize().toString("hex")}`);
     logger.debug("[/NPSMsg]======================================");
