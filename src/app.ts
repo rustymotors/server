3d51b007d469d9ceb3dfaad17afc39979a2a0b55
Joe Becher <jwbecher@drazisil.com>
2019-07-21 21:47:58 -0400

    create repl

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import { ConfigManager } from "./configManager";
+import * as net from "net";
+import * as repl from "repl";
 
 // get instance of config
 const config = new ConfigManager().getConfig();
@@ -14,3 +16,14 @@ AuthLogin.start();
 import { PatchServer } from "./services/PatchAndShard/patchServer";
 const patchAndShardServer = new PatchServer(config);
 patchAndShardServer.start();
+
+net
+  .createServer(function(socket) {
+    const local = repl.start({
+      prompt: "node via TCP socket> ",
+      input: socket,
+      output: socket,
+    });
+    local.context.server = server;
+  })
+  .listen(5001, "0.0.0.0");
--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ import { MCServer } from "./services/MCServer/MCServer";
 export class Server {
   public config: IServerConfiguration;
   public logger: bunyan;
+  public mcServer!: MCServer;
+  public adminServer!: AdminServer;
 
   public constructor(config: IServerConfiguration) {
     this.logger = bunyan
@@ -21,12 +23,12 @@ export class Server {
     this.logger.info("Starting servers...");
 
     // Start the MC Server
-    const mcServer = new MCServer();
-    await mcServer.startServers(this.config);
+    this.mcServer = new MCServer();
+    this.mcServer.startServers(this.config);
 
     // Start the Admin server
-    const adminServer = new AdminServer(mcServer);
-    adminServer.start(this.config.serverConfig);
+    this.adminServer = new AdminServer(this.mcServer);
+    this.adminServer.start(this.config.serverConfig);
     this.logger.info("[adminServer] Web Server started");
 
     this.logger.info("Servers started, ready for connections.");
