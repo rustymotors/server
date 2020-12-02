1e50a3827d78478b263d823e1d42f8bce2eff0c5
Joe Becher <me@drazisil.com>
2020-12-02 12:58:36 -0500

    Saving

---
+++ src/services/AuthLogin/src/configManager.js
@@ -0,0 +1,39 @@
+// mco-server is a game server, written from scratch, for an old game
+// Copyright (C) <2017-2018>  <Joseph W Becher>
+
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+const fs = require('fs')
+
+const IServerConfiguration = {
+  serverConfig: {
+    ipServer: '',
+    certFilename: '',
+    publicKeyFilename: '',
+    privateKeyFilename: '',
+    registryFilename: ''
+  }
+}
+
+export class ConfigManager {
+  constructor () {
+    this.config = JSON.parse(
+      fs.readFileSync('src/services/AuthLogin/config.json', 'utf8')
+    )
+  }
+
+  /**
+   *
+   * @return {IServerConfiguration}
+   */
+  getConfig () {
+    return this.config
+  }
+}
+
+module.exports = {
+  IServerConfiguration,
+  ConfigManager
+}
--- src/services/AuthLogin/src/loggerManager.js
+++ src/services/AuthLogin/src/loggerManager.js
@@ -5,23 +5,32 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import * as bunyan from "bunyan";
-const BunyanToGelfStream = require("bunyan-gelf");
+const bunyan = require('bunyan')
+const BunyanToGelfStream = require('bunyan-gelf')
 
 export class Logger {
-  public getLogger(service: string) {
+  /**
+   *
+   * @param {string} service
+   * @return {Logger}
+   */
+  getLogger (service) {
     const logger = bunyan
-      .createLogger({ name: "mcoServer" })
-      .child({ module: service });
+      .createLogger({ name: 'mcoServer' })
+      .child({ module: service })
     logger.addStream({
-      type: "raw",
+      type: 'raw',
       stream: new BunyanToGelfStream({
-        host: "graylog",
-        port: 12201,
-      }),
-    });
-    logger.addSerializers(bunyan.stdSerializers);
+        host: 'graylog',
+        port: 12201
+      })
+    })
+    logger.addSerializers(bunyan.stdSerializers)
 
-    return logger;
+    return logger
   }
+}
+
+module.exports = {
+  Logger
 }
