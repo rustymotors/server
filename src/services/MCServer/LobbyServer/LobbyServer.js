4bc01720205d3db1119b9623ad00bf2b0d397d97
Joe Becher <me@drazisil.com>
2020-12-13 13:02:49 -0500

    Deepcode fixes

--- src/services/MCServer/LobbyServer/LobbyServer.js
+++ src/services/MCServer/LobbyServer/LobbyServer.js
@@ -5,7 +5,6 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-const appSettings = require('../../../../config/app-settings')
 const logger = require('../../../shared/logger').child({
   service: 'mcoserver:LobbyServer'
 })
