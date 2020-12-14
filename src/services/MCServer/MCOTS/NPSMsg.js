b2936702a9710a6f844c9bbd62aedb23f78380a8
Joe Becher <me@drazisil.com>
2020-12-14 08:36:31 -0500

    Deepcode fixes

--- src/services/MCServer/MCOTS/NPSMsg.js
+++ src/services/MCServer/MCOTS/NPSMsg.js
@@ -6,7 +6,6 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 const debug = require('debug')('mcoserver:NPSMsg')
-const appSettings = require('../../../../config/app-settings')
 const logger = require('../../../shared/logger')
 
 /**
