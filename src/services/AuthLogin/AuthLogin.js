cb1427b311009ec7df027c86ebbd236d66bca26f
Jo Becher <jwbecher@gmail.com>
2021-02-01 11:28:07 -0500

    Rename debug to match logger

--- src/services/AuthLogin/AuthLogin.js
+++ src/services/AuthLogin/AuthLogin.js
@@ -5,7 +5,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-const debug = require('debug')('mcoserver:webServer')
+const debug = require('debug')('mcoserver:AuthLogin')
 const fs = require('fs')
 const https = require('https')
 const logger = require('../../shared/logger')
