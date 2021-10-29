9f0a203d80ee82bbd0cbeff8ce7f1a5ff9a7e10b
Drazi Crendraven <drazisil@users.noreply.github.com>
2021-10-29 07:19:09 -0400

    chore: remove unused code

--- packages/patch/test/patch-server.test.ts
+++ packages/patch/test/patch-server.test.ts
@@ -5,18 +5,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import { expect, describe, it, beforeEach } from "@jest/globals";
-import request from "supertest";
-import { CastanetResponse, PatchServer } from "../src";
-
-let patchServer: PatchServer;
+import { describe, it, beforeEach } from "@jest/globals";
+import { PatchServer } from "../src";
 
 // TODO: Rewrite tests - https://github.com/drazisil/mco-server/issues/1010
 describe("PatchServer", () => {
-  beforeEach(() => {
-    patchServer = PatchServer.getInstance();
-  });
-
   it.todo("PatchServer");
 
   it.todo("PatchServer - UpdateInfo");
