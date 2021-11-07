acc8393ecc44511fcac32e77bbd768d724659aec
Drazi Crendraven <drazisil@users.noreply.github.com>
2021-11-07 17:30:04 -0500

    chore: update copyright header

--- packages/config/src/ssl-options.ts
+++ packages/config/src/ssl-options.ts
@@ -1,6 +1,6 @@
 // @ts-check
 // mcos is a game server, written from scratch, for an old game
-// Copyright (C) <2017-2018>  <Joseph W Becher>
+// Copyright (C) <2017-2021>  <Drazi Crendraven>
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
