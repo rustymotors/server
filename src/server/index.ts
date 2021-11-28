65e820d5384e00ebca13b60f58d37935c2ef459e
Drazi Crendraven <drazisil@users.noreply.github.com>
2021-11-28 14:49:27 -0500

    correct tomc packet id detection

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 export class MCOS {
   public static isMCOT(inputBuffer: Buffer) {
-    return inputBuffer.toString("utf8", 2, 6) === "MCOT";
+    return inputBuffer.toString("utf8", 2, 6) === "TOMC";
   }
 }
