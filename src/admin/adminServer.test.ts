791f584662be3a4b4091543b53befe922213f59c
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-01-24 08:23:37 -0500

    create tests for admin server

---
+++ src/admin/adminServer.test.ts
@@ -0,0 +1,17 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { getAdminServer } from "./index";
+import { SocketFactory } from '../socket-factory'
+
+describe('AdminServer', () => {
+    test('handleRequest(), when passed an IncommingRequest and a ServerResponse, returns a ServerResponse', () => {
+        // Arrange
+        const testIncommingMessage = new IncomingMessage(SocketFactory.createSocket())
+        const testServerResponse = new ServerResponse(testIncommingMessage)
+
+        // Act
+        const handledRequest = getAdminServer().handleRequest(testIncommingMessage, testServerResponse)
+
+        // Assert
+        expect(handledRequest.req).toStrictEqual(testIncommingMessage)
+    })
+})
