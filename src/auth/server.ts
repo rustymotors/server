e999e25208265557afac823d2cf9a621da2ed89c
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-03-04 21:26:25 -0500

    some structure

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -3,87 +3,198 @@
  * Following https://web.archive.org/web/20220303012053/https://www-archive.mozilla.org/projects/security/pki/nss/ssl/draft02.html
  */
 
-import net, { Socket } from 'net';
+import net, { Socket } from "net";
 
 function _bufferToString(data: Buffer) {
-    let outputStringArray: string[] = []
-    for (let index = 0; index < data.length; index++) {
-        outputStringArray.push(`0x${data[index].toString(16).padStart(2, '0')}`);        
-    }
-    return outputStringArray.join(', ')
+  let outputStringArray: string[] = [];
+  for (let index = 0; index < data.length; index++) {
+    outputStringArray.push(`0x${data[index].toString(16).padStart(2, "0")}`);
+  }
+  return outputStringArray.join(", ");
+}
+
+enum SSLConnectionState {
+  "NEW_CONNECTION",
+  "ERRORED_CONNECTION",
+  "CLIENT-HELLO",
 }
 
 interface SSLRecord {
-    totalHeaderLengthInBytes: 2 | 3
-    bodyBytes?: Buffer
+  totalHeaderLengthInBytes: 2 | 3;
+  bodyBytes?: Buffer;
 }
 
 function parseRecordBody(data: Buffer, bodyOffset: number) {
-    return data.slice((bodyOffset))
+  return data.slice(bodyOffset);
 }
 
 function _checkContentType(firstByte: number) {
-    if (firstByte === 20) { // 0x14
-        return 'TLS Change Cypher Spec'
-    }
-    if (firstByte === 21) { // 0x15
-        return 'TLS Alert'
-    }
-    if (firstByte === 22) { // 0x16
-        return 'TLS Handshake'
-    }
-    if (firstByte === 23) { // 0x17
-        return 'TLS Application Data'
-    }
-    return 'Unknown'
+  if (firstByte === 20) {
+    // 0x14
+    return "TLS Change Cypher Spec";
+  }
+  if (firstByte === 21) {
+    // 0x15
+    return "TLS Alert";
+  }
+  if (firstByte === 22) {
+    // 0x16
+    return "TLS Handshake";
+  }
+  if (firstByte === 23) {
+    // 0x17
+    return "TLS Application Data";
+  }
+  return "Unknown";
 }
 
+function _onData(data: Buffer, connectionRecord: SSLConnectionRecord) {
+  const firstByte = data[0];
 
-function _onData(data: Buffer, socket: Socket) {
+  const dataContentType = _checkContentType(firstByte);
 
-    const firstByte = data[0]
+  if (dataContentType !== "Unknown") {
+    const error = connectionRecord.closeWithError(
+      `Detected packet as valid TLS, ignoring and closing the socket`
+    );
 
-    const dataContentType = _checkContentType(firstByte)
-
-    if (dataContentType !== 'Unknown') {
-        console.log(`Detected packet as valid TLS, ignoring and closing the socket`);
-        socket.end()
-        return
-    }
-    
-    console.log(_checkContentType(firstByte));
-    
-
-    console.log(`MSB of first byte is set: ${Boolean(data[0] && 0x80)}`); 
-
-    const sslRecord: SSLRecord = {
-        totalHeaderLengthInBytes: (data[0] && 0x80) ? 2 : 3
+    if (error) {
+      console.error(error);
     }
 
-    const recordLength = ((data[0] & 0x7f) << 8) | data[1]
+    return;
+  }
 
-    sslRecord.bodyBytes = parseRecordBody(data, sslRecord.totalHeaderLengthInBytes)
-    
-    console.log(_bufferToString(data));
-    
+  console.log(_checkContentType(firstByte));
 
-    console.dir(sslRecord)
+  console.log(`MSB of first byte is set: ${Boolean(data[0] && 0x80)}`);
+
+  const sslRecord: SSLRecord = {
+    totalHeaderLengthInBytes: data[0] && 0x80 ? 2 : 3,
+  };
+
+  const recordLength = ((data[0] & 0x7f) << 8) | data[1];
+
+  sslRecord.bodyBytes = parseRecordBody(
+    data,
+    sslRecord.totalHeaderLengthInBytes
+  );
+
+  console.log(_bufferToString(data));
+
+  console.dir(sslRecord);
 }
 
-const server = net.createServer((c) => {
-    // 'connection' listener.
-    console.log('client connected');
-    c.on('end', () => {
-      console.log("client disconnected\n\n");
-    });
-    c.on("data", (data) => {
-      _onData(data, c);
-    });
-  });
-  server.on('error', (err) => {
-    throw err;
-  });
-  server.listen(443, () => {
-    console.log('server bound');
-  });
+class SSLConnectionRecord {
+  private _socket: Socket;
+  private _connectionState: SSLConnectionState
+  private _id: string = "";
 
+  get id() {
+    return this._id;
+  }
+
+  /**
+   * createConnectionId
+   * Given a Socket, returns a formatted string
+   */
+  public static createConnectionId(socket: Socket): string {
+    return `${socket.remoteAddress}_${socket.remotePort?.toString(10) || "0"}`;
+  }
+
+  constructor(socket: Socket) {
+    this._connectionState = SSLConnectionState.NEW_CONNECTION
+    this._socket = socket;
+    this._id = SSLConnectionRecord.createConnectionId(socket);
+  }
+
+  /**
+   * closeWithError
+   * Closes the underlying Socket, returns an optional error
+   */
+  public closeWithError(error?: string): string | void {
+    this._connectionState = SSLConnectionState.ERRORED_CONNECTION
+    this._socket.end();
+    return error;
+  }
+}
+
+class SSLConnectionManager {
+  private _connections: SSLConnectionRecord[] = [];
+
+  private static _instance: SSLConnectionManager;
+
+  private constructor() {
+    // Intentionally empty
+  }
+
+  /**
+   * getInstance
+   * Returns the static instance of the class
+   */
+  public static getInstance(): SSLConnectionManager {
+    if (typeof SSLConnectionManager._instance /*?*/ === "undefined") {
+      SSLConnectionManager._instance = new SSLConnectionManager();
+    }
+    return SSLConnectionManager._instance; /*?*/
+  }
+
+  /**
+   * addConnection
+   * Add a new SSLConnection record
+   */
+  public addConnection(connection: SSLConnectionRecord): SSLConnectionRecord {
+    this._connections.push(connection);
+    return connection;
+  }
+
+  /**
+   * getConnections
+   * Returns the list of connections as a ReadOnly array
+   */
+  public getConnections(): Readonly<SSLConnectionRecord[]> {
+    return Object.freeze(this._connections);
+  }
+
+  /**
+   * findConnection
+   * Find an exist connection record. Returns undefined if not found
+   */
+  public findConnection(id: string): SSLConnectionRecord | undefined {
+    return this._connections.find((record) => {
+      return record.id === id;
+    });
+  }
+}
+
+function _listener(listeningSocket: Socket) {
+  console.log("client connected");
+
+  // Get instance of SSLConnectionManager
+  const connectionManager = SSLConnectionManager.getInstance();
+
+  let connectionRecordId =
+    SSLConnectionRecord.createConnectionId(listeningSocket);
+
+  const connectionRecord: SSLConnectionRecord =
+    connectionManager.findConnection(connectionRecordId) ||
+    connectionManager.addConnection(new SSLConnectionRecord(listeningSocket));
+
+  console.log(`We have a connection record for id: ${connectionRecord.id}`);
+
+  listeningSocket.on("end", () => {
+    console.log("client disconnected\n\n");
+  });
+  listeningSocket.on("data", (data) => {
+    _onData(data, connectionRecord);
+  });
+}
+
+const server = net.createServer(_listener);
+
+server.on("error", (err) => {
+  throw err;
+});
+server.listen(443, () => {
+  console.log("server bound");
+});
