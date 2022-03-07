3cb0cd4aec4fb187dbabc115e78db0f830151e62
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-03-07 18:58:12 -0500

    add getbit and set bit

--- src/auth/server.ts
+++ src/auth/server.ts
@@ -5,6 +5,29 @@
 
 import net, { Socket } from "net";
 
+
+/**
+ * Credit to https://github.com/trekhleb/javascript-algorithms/tree/master/src/algorithms/math/bits
+ */
+
+/**
+ * @param number 
+ * @param bitPosition 
+ * @returns {1 || 0}
+ */
+export function getBit(number: number, bitPosition: number): number {
+  return (number >> bitPosition) & 1;
+}
+
+/**
+ * @param {number} number
+ * @param {number} bitPosition - zero based.
+ * @return {number}
+ */
+ export function setBit(number: number, bitPosition: number): number {
+  return number | (1 << bitPosition);
+}
+
 function _bufferToString(data: Buffer) {
   let outputStringArray: string[] = [];
   for (let index = 0; index < data.length; index++) {
