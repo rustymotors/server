7f0f9efafad4bf676298a83d890049a83d354c37
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-04-06 11:27:51 -0400

    initial bin object and loby structure

---
+++ services/shared/structures/BinObject.ts
@@ -0,0 +1,32 @@
+export enum EBinaryFieldType {
+    STRING,
+    NUMBER,
+    BOOLEAN
+}
+
+export interface IBinaryField {
+    name: string,
+    type: EBinaryFieldType,
+    size: number,
+    value: Buffer,
+    notes?: string
+}
+
+export class BinObject {
+    protected _byteOrder: 'big' | 'little' = 'little'
+    protected _fields: IBinaryField[] = []
+
+    public getField(fieldname: string): IBinaryField {
+        const selectedField = this._fields.find(field => {
+            return field.name === fieldname
+        })
+
+        if (typeof selectedField === "undefined") {
+            throw new Error(`There was no field located with this name: ${fieldname}`);
+            
+        }
+
+        return selectedField
+    }
+
+}
