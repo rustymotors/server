0651c433c645dd1a437c61b56556eae41c2665bb
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-05-06 13:56:46 -0400

    remove built files

--- packages/mcos-gateway/built/web.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Routes incomming HTTP requests
- * @param {import("node:http").IncomingMessage} req
- * @param {import("node:http").ServerResponse} res
- * @returns {import("node:http").ServerResponse}
- */
-export function httpListener(req: import("node:http").IncomingMessage, res: import("node:http").ServerResponse): import("node:http").ServerResponse;
-//# sourceMappingURL=web.d.ts.map
--- packages/mcos-transactions/built/internal.d.ts
+++
@@ -1,6 +0,0 @@
-/**
-   * @param {import("mcos-shared/types").BufferWithConnection} dataConnection
-   * @return {Promise<import('mcos-shared/types').TSMessageArrayWithConnection>}
-   */
-export function handleData(dataConnection: import("mcos-shared/types").BufferWithConnection): Promise<import('mcos-shared/types').TSMessageArrayWithConnection>;
-//# sourceMappingURL=internal.d.ts.map
