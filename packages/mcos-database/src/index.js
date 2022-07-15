cd5ffefa3d7922a8081dc2ae3e6932493f205733
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-07-14 22:15:11 -0400

    start of server and move config to standalone env

--- packages/mcos-database/src/index.js
+++ packages/mcos-database/src/index.js
@@ -16,7 +16,8 @@
 
 import pg from 'pg'
 import { logger } from 'mcos-shared/logger'
-import { APP_CONFIG } from 'mcos-shared/config'
+import { Session } from "./models/Session.js";
+import { Lobby } from "./models/Lobby.js";
 const { Client } = pg
 
 const log = logger.child({ service: 'mcoserver:DatabaseMgr' })
@@ -60,106 +61,16 @@ export class DatabaseManager {
         const self = DatabaseManager._instance
 
         const db = new Client({
-          connectionString: APP_CONFIG.MCOS.SETTINGS.DATABASE_CONNECTION_URI
+          connectionString: process.env.CONNECTION_URL
         })
 
         await db.connect()
 
         self.localDB = db
 
-        await db.query(`CREATE TABLE IF NOT EXISTS "sessions"
-            (
-              customer_id integer,
-              sessionkey text NOT NULL,
-              skey text NOT NULL,
-              context_id text NOT NULL,
-              connection_id text NOT NULL,
-              CONSTRAINT pk_session PRIMARY KEY(customer_id)
-            );`)
+        await db.query(Session.schema)
 
-        await db.query(`CREATE TABLE IF NOT EXISTS "lobbies"
-            (
-              "lobyID" integer NOT NULL,
-              "raceTypeID" integer NOT NULL,
-              "turfID" integer NOT NULL,
-              "riffName" character(32) NOT NULL,
-              "eTerfName" character(265) NOT NULL,
-              "clientArt" character(11) NOT NULL,
-              "elementID" integer NOT NULL,
-              "terfLength" integer NOT NULL,
-              "startSlice" integer NOT NULL,
-              "endSlice" integer NOT NULL,
-              "dragStageLeft" integer NOT NULL,
-              "dragStageRight" integer NOT NULL,
-              "dragStagingSlice" integer NOT NULL,
-              "gridSpreadFactor" real NOT NULL,
-              "linear" smallint NOT NULL,
-              "numPlayersMin" smallint NOT NULL,
-              "numPlayersMax" smallint NOT NULL,
-              "numPlayersDefault" smallint NOT NULL,
-              "bnumPlayersEnable" smallint NOT NULL,
-              "numLapsMin" smallint NOT NULL,
-              "numLapsMax" smallint NOT NULL,
-              "numLapsDefault" smallint NOT NULL,
-              "bnumLapsEnabled" smallint NOT NULL,
-              "numRoundsMin" smallint NOT NULL,
-              "numRoundsMax" smallint NOT NULL,
-              "numRoundsDefault" smallint NOT NULL,
-              "bnumRoundsEnabled" smallint NOT NULL,
-              "bWeatherDefault" smallint NOT NULL,
-              "bWeatherEnabled" smallint NOT NULL,
-              "bNightDefault" smallint NOT NULL,
-              "bNightEnabled" smallint NOT NULL,
-              "bBackwardDefault" smallint NOT NULL,
-              "bBackwardEnabled" smallint NOT NULL,
-              "bTrafficDefault" smallint NOT NULL,
-              "bTrafficEnabled" smallint NOT NULL,
-              "bDamageDefault" smallint NOT NULL,
-              "bDamageEnabled" smallint NOT NULL,
-              "bAIDefault" smallint NOT NULL,
-              "bAIEnabled" smallint NOT NULL,
-              "topDog" character(13) NOT NULL,
-              "terfOwner" character(33) NOT NULL,
-              "qualifingTime" integer NOT NULL,
-              "clubNumPlayers" integer NOT NULL,
-              "clubNumLaps" integer NOT NULL,
-              "clubNumRounds" integer NOT NULL,
-              "bClubNight" smallint NOT NULL,
-              "bClubWeather" smallint NOT NULL,
-              "bClubBackwards" smallint NOT NULL,
-              "topSeedsMP" integer NOT NULL,
-              "lobbyDifficulty" integer NOT NULL,
-              "ttPointForQualify" integer NOT NULL,
-              "ttCashForQualify" integer NOT NULL,
-              "ttPointBonusFasterIncs" integer NOT NULL,
-              "ttCashBonusFasterIncs" integer NOT NULL,
-              "ttTimeIncrements" integer NOT NULL,
-              "victoryPoints1" integer NOT NULL,
-              "victoryCash1" integer NOT NULL,
-              "victoryPoints2" integer NOT NULL,
-              "victoryCash2" integer NOT NULL,
-              "victoryPoints3" integer NOT NULL,
-              "victoryCash3" integer NOT NULL,
-              "minLevel" smallint NOT NULL,
-              "minResetSlice" integer NOT NULL,
-              "maxResetSlice" integer NOT NULL,
-              "bnewbieFlag" smallint NOT NULL,
-              "bdriverHelmetFlag" smallint NOT NULL,
-              "clubNumPlayersMax" smallint NOT NULL,
-              "clubNumPlayersMin" smallint NOT NULL,
-              "clubNumPlayersDefault" smallint NOT NULL,
-              "numClubsMax" smallint NOT NULL,
-              "numClubsMin" smallint NOT NULL,
-              "racePointsFactor" real NOT NULL,
-              "bodyClassMax" smallint NOT NULL,
-              "powerClassMax" smallint NOT NULL,
-              "clubLogoID" integer NOT NULL,
-              "teamtWeather" smallint NOT NULL,
-              "teamtNight" smallint NOT NULL,
-              "teamtBackwards" smallint NOT NULL,
-              "teamtNumLaps" smallint NOT NULL,
-              "raceCashFactor" real NOT NULL
-            );`)
+        await db.query(Lobby.schema)
         log.debug('Database initialized')
       } catch (/** @type {unknown} */ err) {
         if (err instanceof Error) {
@@ -181,10 +92,10 @@ export class DatabaseManager {
    * @memberof DatabaseManager
    */
   constructor () {
-    if (!APP_CONFIG.MCOS.SETTINGS.DATABASE_CONNECTION_URI) {
-      throw new Error('Please set MCOS__SETTINGS__DATABASE_CONNECTION_URI')
+    if (!process.env.CONNECTION_URL) {
+      throw new Error('Please set CONNECTION_URL')
     }
-    this.connectionURI = APP_CONFIG.MCOS.SETTINGS.DATABASE_CONNECTION_URI
+    this.connectionURI = process.env.CONNECTION_URL
     /** @type {pg.Client | undefined} */
     this.localDB = undefined
   }
---
+++ packages/mcos-database/src/server.ts
@@ -0,0 +1,61 @@
+import { createServer, IncomingMessage, ServerResponse } from "http";
+import { logger } from "mcos-shared/logger";
+import { RequestHandler, ResponseJSON } from "./types.js";
+
+const log = logger.child({ service: "mcoserver:DatabaseMgr" });
+
+const server = createServer(onRequest);
+
+const handlers: RequestHandler[] = [];
+
+function onRequest(request: IncomingMessage, response: ServerResponse) {
+  const { "content-type": contentType } = request.headers;
+
+  if (contentType !== "application/jason") {
+    response.statusCode = 415;
+    response.end();
+  }
+
+  const { statusCode, responseContent } = handleRequest(request);
+
+  response.statusCode = statusCode
+  response.end(responseContent)
+}
+
+server.on("error", (err: Error) => {
+  log.error(`Server had an error! ${err.message}`);
+});
+function handleRequest(request: IncomingMessage): ResponseJSON {
+  request.setEncoding("utf8");
+  const requestJSON = parseRequest(request.read());
+
+  const requestHandler = handlers.find((handler: RequestHandler) => {
+    return handler.requestCode === requestJSON.requestCode;
+  });
+
+  // Initialize return with failure
+  let returnJSON: ResponseJSON = {
+    statusCode: 404,
+    responseContent: { error: "Unsupported request", message: "" },
+  };
+
+  // If we located a matching handler, call it
+  if (typeof requestHandler !== "undefined") {
+   const responseJSON = requestHandler.handlerFunction(requestJSON)
+   returnJSON = {
+    statusCode: responseJSON.statusCode,
+    responseContent: responseJSON.responseContent
+   }
+  }
+  return returnJSON
+}
+function parseRequest(requestString: string) {
+  try {
+    const json = JSON.parse(requestString);
+    const { requestCode, requestContent } = json;
+    const requestJSON = { requestCode, requestContent };
+    return requestJSON;
+  } catch (error) {
+    throw new Error(`Unknown error while pasing request: ${new String(error)}`);
+  }
+}
