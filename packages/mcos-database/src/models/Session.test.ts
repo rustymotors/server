8e77e93250cf511e5af6170c9b0bc9f8996ecff4
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-07-14 21:16:42 -0400

    add connection session model (#1161)

---
+++ packages/mcos-database/src/models/Session.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai";
+import { Session } from "./Session.js";
+
+describe("Session model", function() {
+  it("should have a schema property", function() {
+    expect(Session.schema).not.equal('')
+  })
+})
