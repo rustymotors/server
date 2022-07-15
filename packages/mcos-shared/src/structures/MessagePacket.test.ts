741cfb8cf27650346c4b0840b9c85c2527400196
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-07-14 21:57:12 -0400

    add tests for message packet class

---
+++ packages/mcos-shared/src/structures/MessagePacket.test.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import { MessagePacket } from "./MessagePacket.js";
+
+describe("MessagePacket class", function() {
+  it("should return an instance of MessagePacket when the status method fromBuffer() is called", function() {
+    // arrange
+    const inputBuffer = Buffer.from([0x00, 0x01, 0x02, 0x03])
+
+    // act
+    const newMessagePacket = MessagePacket.fromBuffer(inputBuffer)
+
+    // assert
+    expect(newMessagePacket).instanceOf(MessagePacket)
+  })
+
+  it("should return it's internal buffer when the buffer property is accessed", function() {
+    // arrange
+    const inputBuffer = Buffer.from([0x03, 0x04, 0x05, 0x06])
+    const expectedBuffer = Buffer.from([0x03, 0x04, 0x05, 0x06])
+
+    // act
+    const newMessagePacket = MessagePacket.fromBuffer(inputBuffer)
+    const buffer = newMessagePacket.buffer
+
+    // assert
+    expect(buffer).equal(expectedBuffer)
+  })
+})
