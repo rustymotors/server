c6867ced3112ed95f3c61a0e6b2b883deb0a3610
Drazi Crendraven <drazisil@hey.com>
2023-04-03 09:40:50 -0400

    Avoid using wildcard imports JS-C1003

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import * as Sentry from "@sentry/node";
+import Sentry from "@sentry/node";
 import { startListeners } from "./packages/mcos-gateway/src/index.js";
 import log from "./log.js";
 
