02694ce34255127357acaa4c06f72dfeb2be768e
Drazi Crendraven <drazisil@hey.com>
2023-04-07 21:12:59 -0400

    Remove unused import

--- packages/mcos-login/src/index.js
+++ packages/mcos-login/src/index.js
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { DatabaseManager } from "../../mcos-database/src/index.js";
 import { handleData } from "./internal.js";
 
 /**
