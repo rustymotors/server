ce419c3f590f34139be62e19f0655cdeebc7da5c
Drazi Crendraven <drazisil@hey.com>
2023-05-03 09:48:47 -0400

    Remove cyclic imports

--- packages/mcos-gateway/src/adminServer.ts
+++ packages/mcos-gateway/src/adminServer.ts
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { getAllConnections } from "./index.js";
+import { getAllConnections } from "./connections.js";
 import { releaseQueue } from "./releaseQueue.js";
 import { listConnections } from "./listConnections.js";
 import { resetQueue } from "./resetQueue.js";
