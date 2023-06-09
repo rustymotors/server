7824a6df25baaadd196f2b201231aabe3353ab5f
Drazi Crendraven <molly.crendraven@gmail.com>
2023-06-09 12:34:13 -0400

    Define type imports
    Signed-off-by: Drazi Crendraven <molly.crendraven@gmail.com>

--- src/shared/BinaryStructure.ts
+++ src/shared/BinaryStructure.ts
@@ -14,7 +14,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import { FIELD_TYPE, Sentry, TServerLogger, toHex } from "mcos/shared";
+import { Sentry, toHex } from "mcos/shared";
+import type { FIELD_TYPE, TServerLogger } from "mcos/shared";
 
 /**
  * @class
