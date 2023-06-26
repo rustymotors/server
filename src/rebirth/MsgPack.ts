b44769f984ca1b392230bc21e108f49d663122b7
Drazi Crendraven <molly.crendraven@gmail.com>
2023-06-26 08:14:34 -0400

    Correct inclusive language, add size notes
    Signed-off-by: Drazi Crendraven <molly.crendraven@gmail.com>

--- src/rebirth/MsgPack.ts
+++ src/rebirth/MsgPack.ts
@@ -9,15 +9,15 @@
 // "c" is a char (1 byte)
 // "lpb" is a _NPS_UserInfo (long, pointer, byte ) (size 108)
 // "lp" ia _NPS_MiniUserInfo (long, pointer) (size 40)
-// "l" is a _NPS_UserId_List
-// "llpppb" is a _NPS_LoginInfo, size _NPS_UserInfo + 147
-// "ll" is a _NPS_ClientCount
+// "l" is a _NPS_UserId_List (long) (size 4)
+// "llpppb" is a _NPS_LoginInfo, size _NPS_UserInfo + 147 (size 255)
+// "ll" is a _NPS_ClientCount (long, long) (size 8)
 // "lplllllsssssspscssllbl" is a _NPS_CommData
 //      (long, pointer, long, long, long, long, long, short, short, short, short, short, short, pointer, short, char, short, short, long, byte, long)
 //      (size 368)
-// "ll" is a _NPS_ChannelClosed
-// "ll" is a _NPS_ForceLeaveChannel
-// "ll" is a _NPS_UserLocation
+// "ll" is a _NPS_ChannelClosed (long, long) (size 8)
+// "ll" is a _NPS_ForceLeaveChannel (long, long) (size 8)
+// "ll" is a _NPS_UserLocation (long, long) (size 8)
 // "ppp" is a _NPS_GameServersInfo (pointer. pointer, pointer) (size 207)
 // "ll" is a _NPS_GameGerversListheader
 // "pllpssssbsslc" is a _NPS_RiffInfo
@@ -30,19 +30,19 @@
 // "ll" is a _NPS_ChannelGrantedData (long, long) (size 8)
 // "plplll" is a _NPS_RunningServerInfo
 // "ll" is a _NPS_UserLeftChannel (long, long) (size 8)
-// "slll" is a NPS_SysErr
-// "llls" is a _NPS_SlotInfo
+// "slll" is a NPS_SysErr (short, long, long, long) (size 14)
+// "llls" is a _NPS_SlotInfo (long, long, long, short) (size 16)
 // "llss" is a _NPS_ReadyForGame (long, long, short, short) (size 12)
 // "lplbss" is a _NPS_ChannelCreated
 // "lplb" is a _NPS_UserJoinedChannel
 // "ls" is a _ReadyForGameInfo
-// "lll" is a _NPS_Statisticsheader
+// "lll" is a _NPS_Statisticsheader (long, long, long) (size 12)
 // "ll" is a _NPS_SetCommChannelFlags (long, long) (size 8)
 // "ls" is a _NPS_StartGame (long, short) (size 6)
 // "lp" is a _NPS_GameServerLaunchInfo (long, pointer) (size 24)
 // "llc" is a _NPS_GameServerStateInfo
 // "ll" is a _NPS_UDPChannelStatus (long, long) (size 8)
-// "ll" is a _NPS_ChannelMaster
+// "ll" is a _NPS_ChannelAdmin (long, long) (size 8)
 
 // "ss" is a _NPS_Qposition, size = short + short = 4
 // short position;
@@ -50,4 +50,4 @@
 
 // "l" is a _NPS_LocateUser, (long) (size 4)
 
-// "ll" is a SecondHeader_t
+// "ll" is a SecondHeader_t (long, long) (size 8)
