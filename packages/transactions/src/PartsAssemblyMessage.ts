660ac3b73f1c491f47aaba2ee3246343377c00be
Drazi Crendraven <molly.crendraven@gmail.com>
2023-10-13 16:47:46 -0400

    fix size, add missing serialize method
    Signed-off-by: Drazi Crendraven <molly.crendraven@gmail.com>

--- packages/transactions/src/PartsAssemblyMessage.ts
+++ packages/transactions/src/PartsAssemblyMessage.ts
@@ -11,11 +11,29 @@ export class PartsAssemblyMessage extends SerializedBuffer {
         this._ownerId = owerId; // 4 bytes
         this._numberOfParts = 0; // 1 bytes
         /** @type {Part[]} */
-        this._partList = []; // 8 bytes each
+        this._partList = []; // 34 bytes each
     }
 
     override size() {
-        return 5 + this._partList.length * 8;
+        return 7 + this._partList.length * 34;
+    }
+
+    override serialize() {
+        const buffer = Buffer.alloc(this.size());
+        let offset = 0; // offset is 0
+        buffer.writeUInt16LE(this._msgNo, offset);
+        offset += 2; // offset is 2
+        buffer.writeUInt32LE(this._ownerId, offset);
+        offset += 4; // offset is 6
+        buffer.writeInt8(this._numberOfParts, offset);
+        offset += 1; // offset is 7
+        for (const part of this._partList) {
+            const partBuffer = part.serialize();
+            partBuffer.copy(buffer, offset);
+            offset += partBuffer.length;
+        }
+
+        return buffer;
     }
 }
 
@@ -43,34 +61,36 @@ export class Part extends SerializedBuffer {
         this._damage = 0; // 1 byte
         this._retailPrice = 0; // 4 bytes
         this._maxWear = 0; // 4 bytes
+        // 33 bytes total
     }
 
     override size() {
-        return 33;
+        return 34;
     }
 
     override serialize() {
         const buffer = Buffer.alloc(this.size());
         let offset = 0;
         buffer.writeUInt32LE(this._partId, offset);
-        offset += 4;
+        offset += 4; // offset is 4
         buffer.writeUInt32LE(this._parentPartId, offset);
-        offset += 4;
+        offset += 4; // offset is 8
         buffer.writeUInt32LE(this._brandedPartId, offset);
-        offset += 4;
+        offset += 4; // offset is 12
         buffer.writeUInt32LE(this._repairPrice, offset);
-        offset += 4;
+        offset += 4; // offset is 16
         buffer.writeUInt32LE(this._junkPrice, offset);
-        offset += 4;
+        offset += 4; // offset is 20
         buffer.writeUInt32LE(this._wear, offset);
-        offset += 4;
+        offset += 4; // offset is 24
         buffer.writeUInt8(this._attachmentPoint, offset);
-        offset += 1;
+        offset += 1; // offset is 25
         buffer.writeUInt8(this._damage, offset);
-        offset += 1;
+        offset += 1; // offset is 26
         buffer.writeUInt32LE(this._retailPrice, offset);
-        offset += 4;
+        offset += 4; // offset is 30
         buffer.writeUInt32LE(this._maxWear, offset);
+        // offset += 4; // offset is 34
 
         return buffer;
     }
