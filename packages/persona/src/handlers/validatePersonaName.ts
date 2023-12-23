27d28e4bf52d2028294e1074a8318d0abf23b8d4
Molly Crendraven <molly.crendraven@gmail.com>
2023-12-23 09:23:22 -0500

    formatting

--- packages/persona/src/handlers/validatePersonaName.ts
+++ packages/persona/src/handlers/validatePersonaName.ts
@@ -1,8 +1,9 @@
 import { getServerLogger } from "../../../shared/log.js";
-import { LegacyMessage, SerializedBuffer } from "../../../shared/messageFactory.js";
+import {
+    LegacyMessage,
+    SerializedBuffer,
+} from "../../../shared/messageFactory.js";
 import { RawMessage } from "../../../shared/src/RawMessage.js";
-
-
 
 /**
  * Check if a new persona name is valid
@@ -32,7 +33,7 @@ export async function validatePersonaName({
     );
 
     // Build the packet
-    const responsePacket = new RawMessage(522); // 0x020a - NPS_DUP_USER  
+    const responsePacket = new RawMessage(522); // 0x020a - NPS_DUP_USER
     log.debug(
         `NPSMsg response object from _npsLogoutGameUser',
       ${JSON.stringify({
