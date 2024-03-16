7f3972ac6180f03d64a311e089a6b83f9f18e68e
Molly Crendraven <drazi@duck.com>
2024-03-16 00:52:37 -0400

    This doesn't throw an error

--- packages/shared/SubThread.ts
+++ packages/shared/SubThread.ts
@@ -21,7 +21,6 @@ export class SubThread extends EventEmitter {
 
     init() {
         this.emit("initialized");
-        // @ts-expect-error
         this.timer = setInterval(this.run.bind(this), this.loopInterval);
     }
 
