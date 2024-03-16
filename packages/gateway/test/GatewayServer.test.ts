30692680a74599c1ca3d3e0b179ed786c6d2f6d1
Molly Draven <drazi@duck.com>
2024-03-16 01:43:45 -0400

    Refactor getGatewayServer tests to include single port scenario

--- packages/gateway/test/GatewayServer.test.ts
+++ packages/gateway/test/GatewayServer.test.ts
@@ -4,7 +4,7 @@ import type { GatewayOptions } from "../src/GatewayServer.js";
 import { describe, it, expect, vi } from "vitest";
 
 describe("getGatewayServer", () => {
-    it("should be able to get a singleton instance", () => {
+    it("should be able to get a singleton instance when called with multiple ports", () => {
         // Arrange
         const log: TServerLogger = {
             debug: () => vi.fn(),
@@ -18,6 +18,30 @@ describe("getGatewayServer", () => {
         const options: GatewayOptions = {
             log,
             listeningPortList: [1234, 5678],
+        };
+
+        // Act
+        const gateway = getGatewayServer(options);
+
+        // Assert
+        expect(gateway).toBeDefined();
+        expect(gateway).toBe(getGatewayServer(options));
+    });
+
+    it("should be able to get a singleton instance when called with a single port", () => {
+        // Arrange
+        const log: TServerLogger = {
+            debug: () => vi.fn(),
+            error: () => vi.fn(),
+            info: () => vi.fn(),
+            warn: () => vi.fn(),
+            fatal: () => vi.fn(),
+            trace: () => vi.fn(),
+        };
+
+        const options: GatewayOptions = {
+            log,
+            listeningPortList: [1234],
         };
 
         // Act
