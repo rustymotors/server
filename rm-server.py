35b9ee74a33e92775bf3f421333be86075a0f1fd
Molly Draven <drazi@duck.com>
2024-06-26 13:17:15 -0400

    chore(deps): add sentry

--- rm-server.py
+++ rm-server.py
@@ -3,6 +3,19 @@
 
 from server.rusty_motors_server import RustyMotorsServer
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://3d28b6085dabc6e8ecd7b263df477ead@o1413557.ingest.us.sentry.io/4507356101476352",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for tracing.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
+
 
 path = "/home/drazisil/Downloads/MCO/mcity.exe"
 
