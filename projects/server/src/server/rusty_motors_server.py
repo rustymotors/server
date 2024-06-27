ef85ebb5fbf1f02fba898dc37aa2965a089e6a17
Molly Draven <drazi@duck.com>
2024-06-27 07:54:19 -0400

    refactor: add more gui and logger

--- projects/server/src/server/rusty_motors_server.py
+++ projects/server/src/server/rusty_motors_server.py
@@ -4,6 +4,7 @@ from .env_default_action import EnvDefault
 from select import poll, POLLIN
 from socketserver import BaseRequestHandler, ThreadingTCPServer
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
+import sys
 
 
 class WebRequestHandler(BaseHTTPRequestHandler):
@@ -28,14 +29,97 @@ class RustyMotorsServer(tk.Frame):
         self,
         master=None,
     ):
+        self.args = self.parseServerArguments()
+
+        self.initialize_gui_and_servers(master)
+
+    def initialize_gui_and_servers(self, master):
+        self.initialize_gui(master)
+
+        self.registerKeyboardBinding()
+
+        self.initializeServers()
+
+    def initialize_gui(self, master):
         tk.Frame.__init__(self, master)
         self.grid()
-        tk.Label(self, text="Hello World!").grid(column=0, row=0)
-        tk.Button(self, text="Quit", command=self.shutdown).grid(column=1, row=0)
-        tk.Button(self, text="Start", command=self.crash).grid(column=2, row=0)
+        tk.Wm.title(self.master, "Rusty Motors Server")
+        tk.Label(self, text="Rusty Motors Server").grid(row=0, column=0)
+        tk.Button(self, text="Quit", command=self.shutdown).grid(row=1, column=1)
+        # Create a Label for log messages
+        self.log = tk.Label(self, text="Log messages will appear here")
+        # Create a scrollbar for the log
+        self.scrollbar = tk.Scrollbar(self, orient="vertical")
+        # Create a read-only Text widget to display the log
+        self.log_text = tk.Text(
+            self,
+            height=10,
+            width=50,
+            yscrollcommand=self.scrollbar.set,
+            wrap="word",
+        )
+        # Create an entry widget for user input
+        self.entry = tk.Entry(self, width=50, invcmd=self.processUserInput)
 
-        self.bindings()
+        # Place the widgets on the grid
+        self.log.grid(row=2, column=0)
+        self.scrollbar.grid(row=3, column=1, sticky="ns")
+        self.log_text.grid(row=3, column=0)
+        self.entry.grid(row=4, column=0)
+        # Configure the scrollbar to scroll the log_text widget
+        self.scrollbar.config(command=self.log_text.yview)
 
+        # Redirect stdout to the log_text widget
+        self.messageLogger()
+
+    def processUserInput(self):
+        self.log_text.insert(tk.END, self.entry.get(), "green")
+
+    def messageLogger(self):
+        class StdoutRedirector:
+            def __init__(self, text_widget, message_type="stdout"):
+                self.text_widget = text_widget
+
+                if message_type == "stdout":
+                    self.color = "black"
+                elif message_type == "stderr":
+                    self.color = "red"
+
+            def write(self, message):
+                self.text_widget.insert(tk.END, message, self.color)
+                self.text_widget.see(tk.END)
+
+        sys.stdout = StdoutRedirector(self.log_text, message_type="stdout")
+        sys.stderr = StdoutRedirector(self.log_text, message_type="stderr")
+
+    def initializeServers(self):
+        # Setup HTTP server
+        self.http_server = ThreadingHTTPServer(
+            (self.args.server_address, self.args.port), WebRequestHandler
+        )
+
+        self.login_server = ThreadingTCPServer(("localhost", 8226), TCPRequestHandler)
+        self.persona_server = ThreadingTCPServer(("localhost", 8228), TCPRequestHandler)
+        self.lobby_server = ThreadingTCPServer(("localhost", 7003), TCPRequestHandler)
+        self.mcots_server = ThreadingTCPServer(("localhost", 43300), TCPRequestHandler)
+
+        self.fdMapper: dict[int, ThreadingHTTPServer | TCPRequestHandler] = {
+            self.http_server.socket.fileno(): self.http_server,
+            self.login_server.fileno(): self.login_server,
+            self.persona_server.fileno(): self.persona_server,
+            self.lobby_server.fileno(): self.lobby_server,
+            self.mcots_server.fileno(): self.mcots_server,
+        }
+
+        self.poller = poll()
+        self.poller.register(self.http_server.socket, POLLIN)
+        self.poller.register(self.login_server, POLLIN)
+        self.poller.register(self.persona_server, POLLIN)
+        self.poller.register(self.lobby_server, POLLIN)
+        self.poller.register(self.mcots_server, POLLIN)
+        print("Registered all sockets")
+
+    def parseServerArguments(self):
         parser = argparse.ArgumentParser()
         parser.add_argument(
             "--server-address",
@@ -61,64 +145,41 @@ class RustyMotorsServer(tk.Frame):
             Can be set with the EXTERNAL_HOST environment variable.",
         )
         args = parser.parse_args()
-
-        # Setup HTTP server
-        self.http_server = ThreadingHTTPServer(
-            (args.server_address, args.port), WebRequestHandler
-        )
-        self.http_server.socket.fileno()
-
-        self.login_server = ThreadingTCPServer(("localhost", 8226), TCPRequestHandler)
-        self.persona_server = ThreadingTCPServer(("localhost", 8228), TCPRequestHandler)
-        self.lobby_server = ThreadingTCPServer(("localhost", 7003), TCPRequestHandler)
-        self.mcots_server = ThreadingTCPServer(("localhost", 43300), TCPRequestHandler)
-
-        self.fdMapper: dict[int, ThreadingHTTPServer | TCPRequestHandler] = {
-            self.http_server.socket.fileno(): self.http_server,
-            self.login_server.fileno(): self.login_server,
-            self.persona_server.fileno(): self.persona_server,
-            self.lobby_server.fileno(): self.lobby_server,
-            self.mcots_server.fileno(): self.mcots_server,
-        }
-
-        self.poller = poll()
-        self.poller.register(self.http_server.socket, POLLIN)
-        self.poller.register(self.login_server, POLLIN)
-        self.poller.register(self.persona_server, POLLIN)
-        self.poller.register(self.lobby_server, POLLIN)
-        self.poller.register(self.mcots_server, POLLIN)
-        print("Registered all sockets")
-
-        self.after(1000, self.try_poll)
-
-    def crash(self):
-        print("Crashing the server")
-        self.data = 1 / 0
+        return args
 
     def shutdown(self):
         print("Shutting down server")
+        self.http_server.server_close()
+        self.login_server.server_close()
+        self.persona_server.server_close()
+        self.lobby_server.server_close()
+        self.mcots_server.server_close()
         self.quit()
 
-    def try_poll(self):
+    def pollingProcess(self):
         incoming = self.poller.poll(1)
 
         if len(incoming) == 0:
-            self.after(1000, self.try_poll)
+            self.after(1000, self.pollingProcess)
             return
 
         for fd, event in incoming:
             if event == POLLIN:
                 self.handle_incoming(fd)
 
-        self.after(1000, self.try_poll)
+        self.after(1000, self.pollingProcess)
 
     def handle_incoming(self, fd):
         server = self.fdMapper[fd]
         print(f"Handling incoming connection from {server}")
         server.handle_request()
 
-    def bindings(self):
+    def registerKeyboardBinding(self):
         self.master.bind("x", lambda event: self.shutdown())
+        # On attempting to focuse on the log_entry widget, the focus will be set to the entry widget
+        self.log_text.bind("<FocusIn>", lambda event: self.entry.focus_set())
+        self.entry.bind("<Return>", lambda event: self.processUserInput())
 
     def run(self):
+        self.after(1000, self.pollingProcess)
         self.mainloop()
--- projects/server/tests/rusty_motors_server_test.py
+++ projects/server/tests/rusty_motors_server_test.py
@@ -1,9 +1,28 @@
-from unittest.mock import patch
+import sys
+from tkinter import Text
+import unittest
+from unittest.mock import MagicMock, patch
 from server.rusty_motors_server import RustyMotorsServer
 
 
-def test_shutdown():
-    server = RustyMotorsServer()
-    with patch.object(server, "quit") as mock_quit:
+class TestRustyMotorsServer(unittest.TestCase):
+    def test_shutdown(self):
+        server = RustyMotorsServer()
+        with patch.object(server, "quit") as mock_quit:
+            server.shutdown()
+            mock_quit.assert_called_once()
+
+    def test_messageLogger(self):
+
+        # Create an instance of RustyMotorsServer
+        server = RustyMotorsServer()
+        # Create a mock Text widget
+        server.log_text = MagicMock(spec=Text)
+
+        # Call the messageLogger method with the mock Text widget
+        server.messageLogger()
+
+        # Verify that sys.stdout and sys.stderr are redirected to the mock Text widget
+        self.assertEqual(sys.stdout.text_widget, server.log_text)
+        self.assertEqual(sys.stderr.text_widget, server.log_text)
         server.shutdown()
-        mock_quit.assert_called_once()
