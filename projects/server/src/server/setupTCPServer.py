236e341fada0d38e7a5cce9808cc64cb293b3010
Molly Draven <drazi@duck.com>
2024-06-28 08:54:03 -0400

    refactor: Update setupTCPServer to allow custom handler

--- projects/server/src/server/setupTCPServer.py
+++ projects/server/src/server/setupTCPServer.py
@@ -5,8 +5,10 @@ from server.tkservers import tkTCPServer
 import tkinter as tk
 
 
-def setupTCPServer(tkApp: tk.Frame, port: int, handler) -> tkTCPServer:
-    server = tkTCPServer(("localhost", port), TCPRequestHandler, tkApp)
+def setupTCPServer(
+    tkApp: tk.Frame, port: int, handler=TCPRequestHandler
+) -> tkTCPServer:
+    server = tkTCPServer(("localhost", port), handler, tkApp)
     server.allow_reuse_address = True
     server.server_bind()
     server.socket.listen(5)
