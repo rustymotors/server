27925d426ae6896c5a6eb2fc2676523c2c5a0508
Molly Draven <drazi@duck.com>
2024-06-28 20:18:38 -0400

    refactor: Improve logging in TCPRequestHandler and WebRequestHandler

--- projects/server/src/server/WebRequestHandler.py
+++ projects/server/src/server/WebRequestHandler.py
@@ -2,19 +2,69 @@ from http.server import BaseHTTPRequestHandler
 from typing import Any
 
 
+class AuthLoginHandler:
+
+    def __init__(self, request_handler: BaseHTTPRequestHandler, app: Any):
+        self.request_handler = request_handler
+        self.app = app
+
+    def do_GET(self):
+
+        print("AuthLoginHandler")
+        print(self.request_handler.query)  # type: ignore
+
+        self.request_handler.send_response(200)
+        self.request_handler.send_header("Content-type", "text/html")
+        self.request_handler.end_headers()
+        self.request_handler.wfile.write(b"Hello, world!")
+
+
+urls = {
+    "/AuthLogin": AuthLoginHandler,
+}
+
+
 class WebRequestHandler(BaseHTTPRequestHandler):
 
     def log_message(self, format: str, *args: Any) -> None:
         if self.server.app.canLog:  # type: ignore
             self.server.app.log(format % args)  # type: ignore
-        return super().log_message(format, *args)
+        else:
+            super().log_message(format, *args)
+
+    def do_POST(self):
+        pass
+
+    def do_PUT(self):
+        pass
+
+    def do_DELETE(self):
+        pass
+
+    def do_HEAD(self):
+        pass
 
     def do_GET(self):
-        try:
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            self.wfile.write(b"Hello World!")
-        except Exception as e:
-            self.send_error(500, f"Server error: {e}")
-            return
+
+        self.query = dict()
+
+        query = dict()
+        if "?" in self.path:
+            self.extract_query_params(query)
+
+        if self.path in urls:
+            try:
+                self.log_message(f"GET request,\nPath: {self.path}\n")
+                handler = urls[self.path]
+                handler(self, self.server.app).do_GET()  # type: ignore
+            except Exception as e:
+                if self.server.app.canLog:  # type: ignore
+                    self.server.app.log(f"Server error: {e}")  # type: ignore
+                self.send_error(500, "Server error")
+
+    def extract_query_params(self, query):
+        self.path, query_string = self.path.split("?", 1)
+        for key_value in query_string.split("&"):
+            key, value = key_value.split("=")
+            query[key] = value
+        self.query = query
