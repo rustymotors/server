ade5ddbe053581197d0d42229b8eccd637a69817
Molly Draven <drazi@duck.com>
2024-08-24 22:45:46 -0400

    refactor: Add ServerError to shared exports

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -5,6 +5,7 @@ export { Configuration, getServerConfiguration } from "./Configuration.js";
 export { SerializedBuffer } from "./src/SerializedBuffer.js";
 export { RawMessage } from "./src/RawMessage.js";
 export { ServerMessage } from "./src/ServerMessage.js";
+export { ServerError } from "./errors/ServerError.js";
 
 export interface KeypressEvent {
 	sequence: string;
