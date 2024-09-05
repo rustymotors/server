0ef11da76504a8e917f9743aa1cc5ca56c68c68b
Molly Draven <drazi@duck.com>
2024-09-05 07:50:39 -0400

    chore: Remove unused MockNPSMessage test file

--- packages/login/test/index.test.ts
+++ packages/login/test/index.test.ts
@@ -1,7 +1,6 @@
 import type { NPSMessage } from "rusty-motors-shared";
 import { handleLoginData } from "../src/internal.js";
 import { describe, expect, it } from "vitest";
-import { MockNPSMessage } from "./MockNPSMessage.js";
 
 it("needs tests", () => {
     expect(true).toBe(true);
