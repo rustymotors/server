d7cb81d1e97859e700890f3e0f2a2923d88f5136
Molly Draven <drazi@duck.com>
2024-09-27 19:53:13 -0400

    test: :white_check_mark: enable tests for chat module

--- packages/persona/src/handlers/getPersonaInfo.ts
+++ packages/persona/src/handlers/getPersonaInfo.ts
@@ -3,11 +3,23 @@ import { createGameProfile } from "rusty-motors-nps";
 import { getPersonasByPersonaId } from "../getPersonasByPersonaId.js";
 import { personaToString } from "../internal.js";
 
+/**
+ * Retrieves persona information based on the provided connection ID and message.
+ *
+ * @param {Object} params - The parameters for the function.
+ * @param {string} params.connectionId - The connection ID associated with the request.
+ * @param {LegacyMessage} params.message - The legacy message containing the request data.
+ * @param {ServerLogger} [params.log] - Optional logger for logging debug information.
+ *
+ * @returns {Promise<Object>} A promise that resolves to an object containing the connection ID and an array of serialized messages.
+ *
+ * @throws {Error} If the persona is not found for the given persona ID.
+ */
 export async function getPersonaInfo({
 	connectionId,
 	message,
 	log = getServerLogger({
-		name: "PersonaServer",
+		name: "persona.getPersonaInfo",
 	}),
 }: {
 	connectionId: string;
