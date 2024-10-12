4cc5a3616042cada910d01db995ba3bd78e8a4b6
Molly Draven <drazi@duck.com>
2024-10-12 10:23:13 -0400

    refactor(mcotsPortRouter): handle error when parsing initial message

--- packages/gateway/src/mcotsPortRouter.ts
+++ packages/gateway/src/mcotsPortRouter.ts
@@ -35,19 +35,23 @@ export async function mcotsPortRouter({
 
 	// Handle the socket connection here
 	socket.on("data", (data) => {
-		log.debug(`[${id}] Received data: ${data.toString("hex")}`);
-		const initialPacket = parseInitialMessage(data);
-		log.debug(`[${id}] Initial packet(str): ${initialPacket}`);
-		log.debug(`[${id}] initial Packet(hex): ${initialPacket.toHexString()}`);
-		routeInitialMessage(id, port, initialPacket)
-			.then((response) => {
-				// Send the response back to the client
-				log.debug(`[${id}] Sending response: ${response.toString("hex")}`);
-				socket.write(response);
-			})
-			.catch((error) => {
-				log.error(`[${id}] Error routing initial message: ${error}`);
-			});
+		try {
+			log.debug(`[${id}] Received data: ${data.toString("hex")}`);
+			const initialPacket = parseInitialMessage(data);
+			log.debug(`[${id}] Initial packet(str): ${initialPacket}`);
+			log.debug(`[${id}] initial Packet(hex): ${initialPacket.toHexString()}`);
+			routeInitialMessage(id, port, initialPacket)
+				.then((response) => {
+					// Send the response back to the client
+					log.debug(`[${id}] Sending response: ${response.toString("hex")}`);
+					socket.write(response);
+				})
+				.catch((error) => {
+					log.error(`[${id}] Error routing initial message: ${error}`);
+				});
+		} catch (error) {
+			log.error(`[${id}] Error parsing initial message: ${error}`);
+		}
 	});
 
 	socket.on("end", () => {
@@ -74,9 +78,7 @@ async function routeInitialMessage(
 	// Route the initial message to the appropriate handler
 	// Messages may be encrypted, this will be handled by the handler
 
-	log.debug(
-		`Routing message for port ${port}: ${initialPacket.toHexString()}`,
-	);
+	log.debug(`Routing message for port ${port}: ${initialPacket.toHexString()}`);
 	let responses: SerializableInterface[] = [];
 
 	switch (port) {
