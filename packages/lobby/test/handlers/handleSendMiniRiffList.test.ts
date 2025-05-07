7632e90c23f48c72136989c9a43dfb6434a0f75d
Molly Draven <drazi@duck.com>
2025-05-07 09:37:44 -0400

    test: update handleSendMiniRiffList test to use BytableMessage and check messageId

--- packages/lobby/test/handlers/handleSendMiniRiffList.test.ts
+++ packages/lobby/test/handlers/handleSendMiniRiffList.test.ts
@@ -1,17 +1,18 @@
 import { describe, expect, it } from "vitest";
 import { LegacyMessage } from "rusty-motors-shared";
 import { handleSendMiniRiffList } from "../../src/handlers/handleSendMiniRiffList.js";
+import { BytableMessage } from "@rustymotors/binary";
 
 describe("handleSendMiniRiffList", () => {
 	it("should return a buffer", async () => {
 		// arrange
-		const incomingMessage = new LegacyMessage();
+		const incomingMessage = new BytableMessage()
 
 		const result = await handleSendMiniRiffList({
 			connectionId: "test",
 			message: incomingMessage,
 		});
 
-		expect(result.message).toBeInstanceOf(LegacyMessage);
+		expect(result.message.header.messageId).toEqual(0x404)
 	});
 });
