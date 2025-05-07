f82d39caee1e4a46efac37ca1efc0af801dd1a28
Molly Draven <drazi@duck.com>
2025-05-06 21:01:53 -0400

    refactor: remove unused import of 'vi' from trackingPing test file

--- packages/transactions/test/trackingPing.test.ts
+++ packages/transactions/test/trackingPing.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test, vi } from "vitest";
+import { describe, expect, test } from "vitest";
 import { OldServerMessage } from "rusty-motors-shared";
 import { trackingPing } from "../src/trackingPing.js";
 
