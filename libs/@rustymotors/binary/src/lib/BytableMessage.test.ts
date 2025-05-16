c9347e52562eee5e8329f3bc1de2f024b0abec90
Molly Draven <drazi@duck.com>
2025-05-16 01:37:49 -0400

    refactor: add licensing information and update import paths to use .js extensions for consistency

--- libs/@rustymotors/binary/src/lib/BytableMessage.test.ts
+++ libs/@rustymotors/binary/src/lib/BytableMessage.test.ts
@@ -1,6 +1,22 @@
+// mcos is a game server, written from scratch, for an old game
+// Copyright (C) <2017>  <Drazi Crendraven>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import { describe, it, expect, beforeEach } from 'vitest';
-import { BytableStructure, BytableFieldTypes } from './BytableMessage';
-import { BytableObject } from './types';
+import { BytableStructure, BytableFieldTypes } from './BytableMessage.js';
+import { BytableObject } from './types.js';
 
 class DummyField extends BytableFieldTypes.Buffer {
     public override value_: Buffer = Buffer.from([0x01, 0x02, 0x03]);
